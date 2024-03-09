ab7b753a61f4001f20a81a235d73af5cd40c640e
Daniel-Rue <78970069+Daniel-Rue@users.noreply.github.com>
2024-03-09 16:04:21 +0300

    Completed hw4

---
+++ migrations/init_schema.sql
@@ -0,0 +1,35 @@
+--liquibase formatted sql
+
+--changeset Daniel-Rue:01-create-link
+CREATE TABLE link
+(
+    id              BIGINT GENERATED ALWAYS AS IDENTITY,
+    url             TEXT NOT NULL,
+    last_check_time TIMESTAMP WITH TIME ZONE NOT NULL,
+    created_at      TIMESTAMP WITH TIME ZONE NOT NULL,
+    created_by      TEXT NOT NULL,
+
+    PRIMARY KEY (id),
+    UNIQUE (url)
+);
+
+--changeset Daniel-Rue:02-create-chat.sql
+CREATE TABLE chat
+(
+    id          BIGINT GENERATED ALWAYS AS IDENTITY,
+    created_at  TIMESTAMP WITH TIME ZONE NOT NULL,
+
+    PRIMARY KEY (id)
+);
+
+--changeset Daniel-Rue:03-create-chat_link.sql
+CREATE TABLE chat_link
+(
+    chat_id        BIGINT NOT NULL,
+    link_id        BIGINT NOT NULL,
+    subscribed_at  TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+
+    PRIMARY KEY (chat_id, link_id),
+    FOREIGN KEY (chat_id) REFERENCES chat (id) ON DELETE CASCADE,
+    FOREIGN KEY (link_id) REFERENCES link (id) ON DELETE CASCADE
+);
--- scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
+++ scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
@@ -1,33 +1,32 @@
 package edu.java.scrapper;
 
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.JdbcDatabaseContainer;
-import org.testcontainers.containers.PostgreSQLContainer;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Testcontainers
-public abstract class IntegrationTest {
-    public static PostgreSQLContainer<?> POSTGRES;
+@SpringBootTest
+public class IntegrationTest extends IntegrationEnvironment {
 
-    static {
-        POSTGRES = new PostgreSQLContainer<>("postgres:15")
-            .withDatabaseName("scrapper")
-            .withUsername("postgres")
-            .withPassword("postgres");
-        POSTGRES.start();
-
-        runMigrations(POSTGRES);
+    @Test
+    public void testTablesCreation() throws Exception {
+        try (Connection connection = POSTGRES.createConnection("");
+             Statement statement = connection.createStatement()) {
+            assertTrue(tableExists(statement, "link"), "Table 'link' was not created.");
+            assertTrue(tableExists(statement, "chat"), "Table 'chat' was not created.");
+            assertTrue(tableExists(statement, "chat_link"), "Table 'chat_link' was not created.");
+        }
     }
 
-    private static void runMigrations(JdbcDatabaseContainer<?> c) {
-        // ...
-    }
-
-    @DynamicPropertySource
-    static void jdbcProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
-        registry.add("spring.datasource.username", POSTGRES::getUsername);
-        registry.add("spring.datasource.password", POSTGRES::getPassword);
+    private boolean tableExists(Statement statement, String tableName) throws Exception {
+        ResultSet rs = statement.executeQuery("SELECT to_regclass('public." + tableName + "');");
+        if (rs.next()) {
+            return rs.getString(1) != null;
+        }
+        return false;
     }
 }
